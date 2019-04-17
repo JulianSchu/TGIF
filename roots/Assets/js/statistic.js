2510febd79053510928c8b4f75413eba9355287a
JulianSchu <jun.xu@hotmail.dem>
2019-04-17 16:20:07 +0200

    It's gone live!

--- roots/Assets/js/statistic.js
+++ roots/Assets/js/statistic.js
@@ -1,12 +1,32 @@
 localStorage.getItem("data")
 
+let url = document.getElementById('source').textContent;
+let myHeader = {
+    headers: {
+        'X-API-Key': 'SWQPv9Ilwk8bHWdt9rlgn7xuEzmbjq3kAxhd5ogr'
+    }
+}
+
+fetch(url, myHeader)
+    .then(res => res.json())
+    .then(function (data) {
+        const dataResults = data.results[0];
+        const people = dataResults.members;
+        console.log(people);
+        getSum(people);
+        tenPercent(statistics, people);
+        createStatsTable(statistics);
+    })
+    .catch(function (err) {
+        console.log(err);
+        alert('Sorry! Something went really wrong. No data will be displayed. There is no response from the source. Maybe they are undergoing a major update. I mean who knows. It\'s America. Everything is possible. So... Please try again later and by later I mean tomorrow or never. BTW I really don\'t like this pop out alert box. But I am too lazy to do anything about it. Have a nice day and may Trump protect you and your family.')
+    })
+
 /////////////////////////////////////////////////
 // Statistics
 
-
 let statistics = {
     totalMembers: 0,
-    sumOfBelow: 0,
     parties: [{
             partyName: 'Republican',
             noMember: 0,
@@ -35,31 +55,32 @@ let statistics = {
     ['ml', []]
     ]
 }
-console.log(statistics)
 
-const dataResults = data.results[0];
-const people = dataResults.members;
+function getSum(obj) {
 
-let Rs = [], Ds = [], Is = [], generalInfo =[Rs, Ds, Is, people]
+    let Rs = [],
+        Ds = [],
+        Is = [],
+        generalInfo = [Rs, Ds, Is, obj]
 
-for (i = 0; i < people.length; i++) {
-    if (people[i].party === 'R') {
-        Rs.push(people[i])
-    } else if (people[i].party === 'D') {
-        Ds.push(people[i])
-    } else if (people[i].party === 'I') {
-        Is.push(people[i])
+    for (i = 0; i < obj.length; i++) {
+        if (obj[i].party === 'R') {
+            Rs.push(obj[i])
+        } else if (obj[i].party === 'D') {
+            Ds.push(obj[i])
+        } else if (obj[i].party === 'I') {
+            Is.push(obj[i])
+        }
     }
+
+    statistics.totalMembers = Rs.length + Ds.length + Is.length;
+
+    statistics.parties.forEach(function (each) {
+        let i = statistics.parties.indexOf(each);
+        each.noMember = generalInfo[i].length;
+        each.vwp_avg = votesWithParty(generalInfo[i]) + "%";
+    })
 }
-
-statistics.totalMembers = people.length;
-statistics.sumOfBelow = Rs.length + Ds.length + Is.length;
-
-statistics.parties.forEach(function(each){
-    let i = statistics.parties.indexOf(each);
-    each.noMember = generalInfo[i].length;
-    each.vwp_avg = votesWithParty(generalInfo[i]) + "%";
-})
 
 function votesWithParty(p) {
     let sumOfVotes = 0;
@@ -78,11 +99,11 @@ function votesWithParty(p) {
 //////////////////////////////////////////////////
 // 10% part
 
-function getLA(a, b) {
+function getLA(a, b, c) {
     if (a.length === 0) {
-        b = people
+        b = c
     } else {
-        b = people.filter(function (re) {
+        b = c.filter(function (re) {
             return re.missed_votes_pct < a[0].missed_votes_pct
         });
     };
@@ -97,11 +118,11 @@ function getLA(a, b) {
     return [a, b];
 }
 
-function getMA(a, b) {
+function getMA(a, b, c) {
     if (a.length === 0) {
-        b = people
+        b = c
     } else {
-        b = people.filter(function (re) {
+        b = c.filter(function (re) {
             return re.missed_votes_pct > a[0].missed_votes_pct
         });
     };
@@ -116,11 +137,11 @@ function getMA(a, b) {
     return [a, b];
 }
 
-function getLL(a, b) {
+function getLL(a, b, c) {
     if (a.length === 0) {
-        b = people
+        b = c
     } else {
-        b = people.filter(function (re) {
+        b = c.filter(function (re) {
             return re.votes_with_party_pct > a[0].votes_with_party_pct
         });
     };
@@ -135,11 +156,11 @@ function getLL(a, b) {
     return [a, b];
 }
 
-function getML(a, b) {
+function getML(a, b, c) {
     if (a.length === 0) {
-        b = people
+        b = c
     } else {
-        b = people.filter(function (re) {
+        b = c.filter(function (re) {
             return re.votes_with_party_pct < a[0].votes_with_party_pct
         });
     };
@@ -156,114 +177,117 @@ function getML(a, b) {
 
 let functions = [getLA, getMA, getLL, getML];
 
-statistics.stats.forEach(function (each) {
-    let i = statistics.stats.indexOf(each);
-    var least, bottoms, x, y, z, rest, restBtms, keepOnSearching, func;
-    least = [];
-    bottoms = [];
-    rest = [];
-    func = functions[i];
-    keepOnSeaching = true;
-    if (i === 0 || i === 1) {
-        while (keepOnSeaching) {
-            keepOnSeaching = false;
-            var inBetween = func(least, rest);
-            least = inBetween[0];
-            rest = inBetween[1];
-            restBtms = rest.filter(function (each) {
-                return each.missed_votes_pct === least[0].missed_votes_pct;
-            })
-            bottoms = bottoms.concat(restBtms)
-            z = bottoms.length / people.length;
-            if (z < 0.1) {
-                keepOnSeaching = true;
-            } else {
-                statistics.stats[i][1] = bottoms.map(function (each) {
-                    return [each.first_name + " " + (each.middle_name || "") + " " + each.last_name, each.missed_votes, each.missed_votes_pct + "%",
+function tenPercent(statistics, c) {
+    statistics.stats.forEach(function (each) {
+        let i = statistics.stats.indexOf(each);
+        var least, bottoms, x, y, z, rest, restBtms, keepOnSearching, func;
+        least = [];
+        bottoms = [];
+        rest = [];
+        func = functions[i];
+        keepOnSeaching = true;
+        if (i === 0 || i === 1) {
+            while (keepOnSeaching) {
+                keepOnSeaching = false;
+                var inBetween = func(least, rest, c);
+                least = inBetween[0];
+                rest = inBetween[1];
+                restBtms = rest.filter(function (each) {
+                    return each.missed_votes_pct === least[0].missed_votes_pct;
+                })
+                bottoms = bottoms.concat(restBtms)
+                z = bottoms.length / c.length;
+                if (z < 0.1) {
+                    keepOnSeaching = true;
+                } else {
+                    statistics.stats[i][1] = bottoms.map(function (each) {
+                        return [each.first_name + " " + (each.middle_name || "") + " " + each.last_name, each.missed_votes, each.missed_votes_pct + "%",
                         each.url]
-                });
+                    });
+                }
+            }
+        } else {
+            while (keepOnSeaching) {
+                keepOnSeaching = false;
+                var inBetween = func(least, rest, c);
+                least = inBetween[0];
+                rest = inBetween[1];
+                restBtms = rest.filter(function (each) {
+                    return each.votes_with_party_pct === least[0].votes_with_party_pct;
+                })
+                bottoms = bottoms.concat(restBtms)
+                z = bottoms.length / c.length;
+                if (z < 0.1) {
+                    keepOnSeaching = true;
+                } else {
+                    statistics.stats[i][1] = bottoms.map(function (each) {
+                        return [each.first_name + " " + (each.middle_name || "") + " " + each.last_name, each.total_votes, each.votes_with_party_pct + "%", each.url]
+                    });
+                }
             }
         }
-    } else {
-        while (keepOnSeaching) {
-            keepOnSeaching = false;
-            var inBetween = func(least, rest);
-            least = inBetween[0];
-            rest = inBetween[1];
-            restBtms = rest.filter(function (each) {
-                return each.votes_with_party_pct === least[0].votes_with_party_pct;
-            })
-            bottoms = bottoms.concat(restBtms)
-            z = bottoms.length / people.length;
-            if (z < 0.1) {
-                keepOnSeaching = true;
-            } else {
-                statistics.stats[i][1] = bottoms.map(function (each) {
-                    return [each.first_name + " " + (each.middle_name || "") + " " + each.last_name, each.total_votes, each.votes_with_party_pct + "%", each.url]
-                });
-            }
-        }
-    }
-});
-
+    });
+}
 
 ///////////////////////////////////////////////
 //create table
 
-let glance = ['glance', []]
+function createStatsTable(statistics) {
+    let glance = ['glance', []]
 
-glance[1] = statistics.parties.map(function (one) {
-    return [one.partyName, one.noMember, one.vwp_avg, '#']
-})
+    glance[1] = statistics.parties.map(function (one) {
+        return [one.partyName, one.noMember, one.vwp_avg, '#']
+    })
 
-let tableInputs = statistics.stats.concat([glance]);
-
-tableInputs.forEach(function (each) {
-    let i = tableInputs.indexOf(each);
-    let table = document.getElementById(i);
-    let tbody = document.createElement('tbody');
-    if (table != null) {
-        each[1].forEach(function (single) {
-            let tr = document.createElement('tr');
-            tr.setAttribute('class', single[0])
+    let tableInputs = statistics.stats.concat([glance]);
+    tableInputs.forEach(function (each) {
+        let i = tableInputs.indexOf(each);
+        let table = document.getElementById(i);
+        let tbody = document.createElement('tbody');
+        if (table != null) {
+            each[1].forEach(function (single) {
+                let tr = document.createElement('tr');
+                tr.setAttribute('class', single[0])
+                if (i != 4) {
+                    let a = document.createElement('a');
+                    a.setAttribute('href', single[single.length - 1]);
+                    a.setAttribute('class', 'text-decoration-none')
+                    a.innerHTML = single[0];
+                    let td = document.createElement('td');
+                    td.appendChild(a);
+                    tr.appendChild(td);
+                } else {
+                    let td = document.createElement('td');
+                    td.innerHTML = single[0];
+                    tr.appendChild(td)
+                };
+                for (n = 1; n < single.length - 1; n++) {
+                    let col = document.createElement('td');
+                    let content = document.createTextNode(single[n]);
+                    col.appendChild(content);
+                    tr.appendChild(col);
+                }
+                tbody.appendChild(tr);
+            })
+            table.appendChild(tbody);
+            let tfoot = document.createElement('tfoot');
+            let noteRow = document.createElement('tr');
+            let note = document.createElement('td');
+            note.setAttribute('colspan', '3');
+            note.setAttribute('class', 'text-left')
+            let small = document.createElement('small');
+            let noteText
             if (i != 4) {
-                let a = document.createElement('a');
-                a.setAttribute('href', single[single.length - 1]);
-                a.setAttribute('class', 'text-decoration-none')
-                a.innerHTML = single[0];
-                let td = document.createElement('td');
-                td.appendChild(a);
-                tr.appendChild(td);
+                noteText = document.createTextNode('*ranking based on %');
             } else {
-                let td = document.createElement('td');
-                td.innerHTML = single[0];
-                tr.appendChild(td)
+                noteText = document.createTextNode('*weighted average')
             };
-            for (n = 1; n < single.length - 1; n++) {
-                let col = document.createElement('td');
-                let content = document.createTextNode(single[n]);
-                col.appendChild(content);
-                tr.appendChild(col);
-            }
-            tbody.appendChild(tr);
-        })
-        table.appendChild(tbody);
-        let tfoot = document.createElement('tfoot');
-        let noteRow = document.createElement('tr');
-        let note = document.createElement('td');
-        note.setAttribute('colspan', '3');
-        note.setAttribute('class', 'text-left')
-        let small = document.createElement('small');
-        let noteText
-        if (i != 4) {
-            noteText = document.createTextNode('*ranking based on %');
-        } else {
-            noteText = document.createTextNode('*weighted average')
-        };
-        small.appendChild(noteText);
-        note.appendChild(small);
-        noteRow.appendChild(note);
-        tfoot.appendChild(noteRow);
-        table.appendChild(tfoot);
-    }
-})
+            small.appendChild(noteText);
+            note.appendChild(small);
+            noteRow.appendChild(note);
+            tfoot.appendChild(noteRow);
+            table.appendChild(tfoot);
+        }
+    })
+
+}
